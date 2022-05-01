c97d2ab559e74ce9fba4460f336dd38e19bfdbab
Daniel Hornik <daniel.1302@gmail.com>
2022-05-01 10:09:32 +0200

    Bump smartcontracts base image

--- smartcontracts-base/contracts-solc-0.8.1/truffle-config.js
+++ smartcontracts-base/contracts-solc-0.8.1/truffle-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   networks: {
     development: {
-      host: process.env.GANACHE_HOSTNAME,
-      port: 8545,
+      host: process.env.GANACHE_HOSTNAME || '127.0.0.1',
+      port: process.env.GANACHE_PORT || 8545,
       network_id: "*",
     },
   },
