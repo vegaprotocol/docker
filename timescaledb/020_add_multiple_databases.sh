500b5eb7464aea48ab1544a8f37ea900fb9048ef
daniel1302 <daniel.1302@gmail.com>
2023-08-30 00:37:16 +0200

    feat: more cleanup

--- timescaledb/020_add_multiple_databases.sh
+++ timescaledb/020_add_multiple_databases.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# ref: https://github.com/bitnami/containers/blob/main/bitnami/postgresql/14/debian-11/rootfs/opt/bitnami/scripts/postgresql/setup.sh
-. /usr/local/bin/docker-entrypoint.sh
-
-
-
 export VEGA_POSTGRES_USER="vega"
 export VEGA_POSTGRES_PASSWORD="vega"
 export VEGA_POSTGRES_DBS="vega0,vega1,vega2,vega3,vega4,vega5,vega6,vega7,vega8,vega9,vega10,vega11,vega12,vega13,vega14,vega15,vega16,vega17,vega18,vega19,vega20,vega21,vega22,vega23,vega24,vega25,vega26,vega27,vega28,vega29"
