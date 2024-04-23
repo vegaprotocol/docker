14b9eb03f78efbec5b2956e9295a8ebc3f3561b4
daniel1302 <daniel.1302@gmail.com>
2024-04-23 16:56:06 +0200

    feat: add support for secondary database

--- smartcontracts-base/contracts-solc-0.8.8/truffle-config.js
+++ smartcontracts-base/contracts-solc-0.8.8/truffle-config.js
@@ -5,6 +5,11 @@ module.exports = {
       port: process.env.GANACHE_PORT || 8545,
       network_id: "*",
     },
+    secondary: {
+      host: process.env.GANACHE_HOSTNAME || '127.0.0.1',
+      port: process.env.GANACHE_PORT_SECONDARY || 8546, 
+      network_id: "1451",
+    },
   },
   compilers: {
     solc: {
