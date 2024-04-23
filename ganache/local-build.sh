4dfa3684589350aaf3060f84700add6994789541
daniel1302 <daniel.1302@gmail.com>
2024-04-23 16:56:06 +0200

    feat: add support for secondary database

--- ganache/local-build.sh
+++ ganache/local-build.sh
@@ -2,13 +2,13 @@
 
 
 cd ../smartcontracts-base
-docker build -t vegaprotocol/smartcontracts-base:v1.4.0 .
+docker build -t vegaprotocol/smartcontracts-base:v1.5.0 .
 
 cd ../ganache
-docker build --progress plain -t vegaprotocol/ganache:v1.4.0 .
+docker build --progress plain -t vegaprotocol/ganache:v1.5.0 .
 
 
-docker run vegaprotocol/ganache:v1.4.0 ganache \
+docker run vegaprotocol/ganache:v1.5.0 ganache \
         --miner.blockTime 0.5 \
         --chain.chainId 1440 \
         --chain.networkId 1441 \
